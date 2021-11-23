3f027809d81dc6391ed93e072d95b94c76e1c863
Wenzhi Ding <wenzhi.ding@foxmail.com>
2021-11-23 14:33:52 +0800

    Fix error

--- 30_Other_Source/M211123-rolling-match-pattern.py
+++ 30_Other_Source/M211123-rolling-match-pattern.py
@@ -18,5 +18,5 @@ pattern = np.array([0] * pre + [1] * post)
 # 特征：array([0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
 
 # 对每个公司滚动应用该判定
-tmp = retm.groupby(['Stkcd'])['treated'].rolling(pre + post).apply(lambda x: (x == pattern).sum())
+tmp = retm.groupby(['Stkcd'])['treated'].rolling(pre + post).apply(lambda x: (x == pattern).sum()).reset_index(drop=True)
 retm.loc[tmp.index, 'mark'] = tmp
