62ca5679a9a7cbd2f2e63160d66f22b0ce00d031
Wenzhi Ding <wenzhi.ding@foxmail.com>
2022-08-14 17:10:11 +0800

    Add str.join

--- 30_Other_Source/M220814_finditer.py
+++ 30_Other_Source/M220814_finditer.py
@@ -1,8 +1,15 @@
 # https://stackoverflow.com/questions/73349913/how-can-i-replace-each-occurrence-of-a-substring-independently-of-the-others
 
+# Solution 1
 import re
 
 sentence = "it is a stupid book and a stupid hat and a stupid computer"
 
 for match in re.finditer(r'\bstupid\b', sentence):
     print(sentence[:match.start()] + 'silly' + sentence[match.end():])
+
+# Solution 2
+def replaceEach(haystack, needle, replacement):
+    pieces = haystack.split(needle)
+    for i in range(len(pieces) - 1):
+        yield needle.join(pieces[:i+1]) + replacement + needle.join(pieces[i+1:])
